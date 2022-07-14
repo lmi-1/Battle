f2410dd29cd1b3ff49afe702cd57730bd119e06b
lmi-1 <lmi-1@rambler.ru>
2022-07-14 20:26:33 +0300

    create classes AI(Player) and User(Player)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class BoardException(Exception):
     pass
 
@@ -149,7 +151,30 @@ class Player:
             except BoardException as e:
                 print(e)
 
+class AI(Player):
+    def ask(self):
+        d = Dot(randint(0, 5), randint(0, 5))
+        print(f"Ход компьютера: {d.x + 1} {d.y + 1}")
+        return d
 
+class User(Player):
+    def ask(self):
+        while True:
+            cords = input("Ваш ход: ").split()
+
+            if len(cords) != 2:
+                print(" Введите 2 координаты! ")
+                continue
+
+            x, y = cords
+
+            if not (x.isdigit()) or not (y.isdigit()):
+                print(" Введите числа! ")
+                continue
+
+            x, y = int(x), int(y)
+
+            return Dot(x - 1, y - 1)
 
 b = Board()
 b.add_ship(Ship(Dot(1, 2), 4, 0))

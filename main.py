bda7565e2b12e312d52437f609ebe86b6ed0641f
lmi-1 <lmi-1@rambler.ru>
2022-07-14 18:07:10 +0300

    create class Board

--- main.py
+++ main.py
@@ -50,5 +50,27 @@ class Ship:
     def shooten(self, shot):
         return shot in self.dots
 
+class Board:
+    def __init__(self, hid=False, size=6):
+        self.size = size
+        self.hid = hid
+
+        self.count = 0
+
+        self.field = [["O"] * size for _ in range(size)]
+
+        self.busy = []
+        self.ships = []
+
+    def __str__(self):
+        res = ""
+        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+        for i, row in enumerate(self.field):
+            res += f"\n{i + 1} | " + " | ".join(row) + " |"
+
+        if self.hid:
+            res = res.replace("■", "O")
+        return res
+    
 s =Ship(Dot(1, 2), 4, 0)
 print(s.shooten(Dot(0, 2)))

3b3e82c070ea2e07721438162eb2ad0406432aaa
lmi-1 <lmi-1@rambler.ru>
2022-07-14 16:33:43 +0300

    create method dots

--- main.py
+++ main.py
@@ -29,3 +29,19 @@ class Ship:
         self.l = l
         self.o = o
         self.lives = l
+
+    def dots(self):
+        ship_dots = []
+        for i in range(self.l):
+            cur_x = self.bow.x
+            cur_y = self.bow.y
+
+            if self.o == 0:
+                cur_x += i
+
+            elif self.o == 1:
+                cur_y += i
+
+            ship_dots.append(Dot(cur_x, cur_y))
+
+        return ship_dots
